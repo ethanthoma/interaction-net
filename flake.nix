b5c9f5c0024247c482a7a21ab078a49ec87f5610
Ethan Thoma <ethanthoma@gmail.com>
2026-05-31 20:37:47 -0700

    chore: upgrade odin to dev-2026-05 and migrate flake to flake-parts/devshell

--- flake.nix
+++ flake.nix
@@ -3,29 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    (flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
+    inputs@{ flake-parts, devshell, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ devshell.flakeModule ];
 
-        odin = pkgs.callPackage ./nix/odin.nix {
-          MacOSX-SDK = pkgs.darwin.apple_sdk;
-          inherit (pkgs.darwin) Security;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages.default = pkgs.callPackage ./nix { };
+
+          devshells.default = {
+            packages = [
+              pkgs.odin
+              pkgs.ols
+            ];
+
+            env = [
+              {
+                name = "ODIN_ROOT";
+                value = "${pkgs.odin}/share";
+              }
+            ];
+          };
         };
-      in
-      {
-        packages.default = pkgs.callPackage ./nix { inherit odin; };
-
-        devShells.default = pkgs.callPackage ./nix/shell.nix { inherit odin; };
-      }
-    ));
+    };
 }
